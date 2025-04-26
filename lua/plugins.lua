7fa7f1af6f863cfc495b07311fe919932e0a2e43
Christian Sarnataro <c.sarnataro@gmail.com>
2025-04-26 14:16:40 +0200

    Initial import. It has basic support for plugins

---
+++ lua/plugins.lua
@@ -0,0 +1,21 @@
+return {
+  -- add your plugins here
+--   {
+--     "nvim-neo-tree/neo-tree.nvim",
+--     branch = "v3.x",
+--     dependencies = {
+--       "nvim-lua/plenary.nvim",
+--       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+--       "MunifTanjim/nui.nvim",
+--       -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+--     },
+--     lazy = false, -- neo-tree will lazily load itself
+--     ---@module "neo-tree"
+--     ---@type neotree.Config?
+--     opts = {
+--       -- fill any relevant options here
+--     },
+--   },
+  -- Configure any other settings here. See the documentation for more details.
+}
+
